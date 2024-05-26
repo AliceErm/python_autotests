c0e751751caa30750f05d3db45832bd04a02a457
AliceErm <166287471+AliceErm@users.noreply.github.com>
2024-05-26 15:20:00 +0600

    Update main.py

--- PythonProjects/main.py
+++ PythonProjects/main.py
@@ -1,7 +1,7 @@
 import requests 
 
 URL = 'https://api.pokemonbattle.me/v2'
-TOKEN = 'dd7f5f9868cae4ccc5975be83f0c8692'
+TOKEN = '123'
 HEADER = {'Content-Type' : 'application/json', 'trainer_token': TOKEN}
 body_pokemon = {
     "name": "Котикc",
